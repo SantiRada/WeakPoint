f0b41b5e09cb110873540eeeeca79b13ac694b4e
Santiago Rada <santynrada@gmail.com>
2026-02-09 04:41:38 -0300

    First Steps
    - Added Template to initial

---
+++ src/main/java/Tenzinn/WeakPoint.java
@@ -0,0 +1,16 @@
+package Tenzinn;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class WeakPoint extends JavaPlugin {
+
+    public WeakPoint(@Nonnull JavaPluginInit init) { super(init); }
+    
+    @Override
+    protected void setup() {
+        
+    }
+}
