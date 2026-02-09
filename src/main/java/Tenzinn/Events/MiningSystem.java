57f428de8677f9a0991bcd1cb3282458269e9e54
Santiago Rada <santynrada@gmail.com>
2026-02-09 18:26:05 -0300

    Floating-point system
    I added the point system that generates points after a hit:
    - This system already includes the particle and its base stats.
    
    I added a sample 3-part particle texture.
    
    To-Do: I'll look in the documentation for how to make the particle always draw above the block.

--- src/main/java/Tenzinn/Events/MiningSystem.java
+++ src/main/java/Tenzinn/Events/MiningSystem.java
@@ -5,22 +5,20 @@ import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.math.vector.Vector3i;
-import com.hypixel.hytale.math.vector.Vector3d;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.ArchetypeChunk;
-import com.hypixel.hytale.server.core.entity.Entity;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.component.system.EntityEventSystem;
+import com.hypixel.hytale.protocol.packets.world.SpawnParticleSystem;
 import com.hypixel.hytale.server.core.event.events.ecs.DamageBlockEvent;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
-import com.hypixel.hytale.server.npc.corecomponents.combat.ActionApplyEntityEffect;
 
 import javax.annotation.Nullable;
 import java.util.Random;
-import java.awt.*;
+import java.awt.Color;
 
 public class MiningSystem extends EntityEventSystem<EntityStore, DamageBlockEvent> {
 
@@ -29,45 +27,64 @@ public class MiningSystem extends EntityEventSystem<EntityStore, DamageBlockEven
     @Override
     public void handle(int i, ArchetypeChunk<EntityStore> chunk, Store<EntityStore> store, CommandBuffer<EntityStore> buffer, DamageBlockEvent event) {
         if (event.getBlockType() == BlockType.EMPTY) return;
-
         if(!event.getBlockType().getId().contains("Ore")) return;
 
         Ref<EntityStore> ref = chunk.getReferenceTo(i);
         Player player = store.getComponent(ref, Player.getComponentType());
-
         if (player == null) return;
+
+        PlayerRef playerRef = store.getComponent(ref, PlayerRef.getComponentType());
+        if (playerRef == null) return;
 
         event.setCancelled(true);
 
         Vector3i baseTransform = event.getTargetBlock();
+        Position particlePosition = calculateNewPosition(baseTransform);
+        Direction particleDirection = calculateNewDirection(baseTransform, particlePosition);
 
+        SpawnParticleSystem packet = new SpawnParticleSystem();
+        packet.particleSystemId = "Weak_Point_Particle";
+        packet.rotation = particleDirection;
+        packet.position = particlePosition;
+        packet.scale = 1.0f;
+
+        player.sendMessage(Message.raw("Direction: (" + particleDirection.yaw + ", " + particleDirection.pitch + ", " + particleDirection.roll + ")").color(Color.cyan));
+        playerRef.getPacketHandler().write(packet);
+    }
+
+    private Position calculateNewPosition(Vector3i baseTransform) {
         Random random = new Random();
         double newX = baseTransform.x + 0.01 + (random.nextDouble() * 0.98);
         double newY = baseTransform.y + 0.01 + (random.nextDouble() * 0.98);
         double newZ = baseTransform.z + 0.01 + (random.nextDouble() * 0.98);
-        Vector3d randomTransform = new Vector3d(newX, newY, newZ);
+        double offset = 0.05f;
 
-        // Como colocarle posición a una partícula...
+        Random randomValue = new Random();
+        int rnd = randomValue.nextInt(100);
+
+        if(rnd >= 0 && rnd < 20) { newX = baseTransform.x + 1 + offset; }
+        else if (rnd >= 20 && rnd < 40) { newX = baseTransform.x - offset; }
+        else if(rnd >= 40 && rnd < 60) { newY = baseTransform.y + 1 + offset; }
+        else if (rnd >= 60 && rnd < 80) { newZ = baseTransform.z + 1 + offset; }
+        else { newZ = baseTransform.z - offset; }
+
+        return new Position(newX, newY, newZ);
     }
+
+    private Direction calculateNewDirection(Vector3i baseTransform, Position pos) {
+        Direction newDirection = new Direction(0.0f, 0.0f, 0.0f);
+
+        if (pos.x <= baseTransform.x) { newDirection.yaw = -90; }
+        else if (pos.x >= baseTransform.x+1) { newDirection.yaw = 90; }
+
+        if (pos.y >= baseTransform.y+1) { newDirection.pitch = 90; }
+
+        if (pos.z <= baseTransform.z) { newDirection.roll = -90; }
+        else if (pos.z >= baseTransform.z+1) { newDirection.roll = 90; }
+
+        return newDirection;
+    }
+
     @Nullable @Override
     public Query<EntityStore> getQuery() { return PlayerRef.getComponentType(); }
-
-    private Particle CreateParticle() {
-        ParticleSpawner spawner = new ParticleSpawner();
-        spawner.particle = new Particle();
-        spawner.particle.texturePath = "Point.png";
-
-        spawner.particle.frameSize.width = 1;
-        spawner.particle.frameSize.height = 1;
-
-        spawner.shape = EmitShape.Sphere;
-        spawner.spawnBurst = true;
-        spawner.totalParticles = new Range(1, 1);
-        spawner.particleLifeSpan = new Rangef(5.0f, 5.0f);
-        spawner.initialVelocity = null;
-
-        spawner.particleRotationInfluence = ParticleRotationInfluence.None;
-
-        return spawner.particle;
-    }
 }
